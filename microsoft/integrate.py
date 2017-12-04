04ee3bc73e6e07b52b281609d9dd42901185f754
gbushong <grace@phoenix.com>
2017-12-04 14:19:32 -0500

    added entity typing task 3 code

--- microsoft/integrate.py
+++ microsoft/integrate.py
@@ -425,6 +425,118 @@ def NaiveBayes():
 		print 'P(NotKDD|X) ~ P(X|NotKDD)P(NotKDD)',0.0001*int(10000.0*PNoPost)
 		print '--> Prediction:',(PYesPost > PNoPost)
 		print ''
+
+def SimpleEntityTyping():
+	n_type = 4
+	s_method = 'method algorithm model approach framework process scheme implementation procedure strategy architecture'
+	s_problem = 'problem technique process system application task evaluation tool paradigm benchmark software'
+	s_dataset = 'data dataset database'
+	s_metric = 'value score measure metric function parameter'
+	types = ['METHOD','PROBLEM','DATASET','METRIC']
+	wordsets = [set(s_method.split(' ')),set(s_problem.split(' ')),set(s_dataset.split(' ')),set(s_metric.split(' '))]
+
+	paperid_path = []
+	fr = open('index.txt','rb')
+	for line in fr:
+		arr = line.strip('\r\n').split('\t')
+		paperid = arr[2]
+		path = '../text/'+arr[0]+'/'+arr[1]+'.txt'
+		paperid_path.append([paperid,path])
+	fr.close()
+	index,nindex = [{}],1 # phrase's index
+	fr = open('phrase2count.txt','rb')
+	for line in fr:
+		arr = line.strip('\r\n').split('\t')
+		phrase = arr[0]
+		words = phrase.split(' ')
+		n = len(words)
+		if n > nindex:
+			for i in range(nindex,n):
+				index.append({})
+			nindex = n
+		temp = index[n-1]
+		if n > 1:
+			for i in range(0,n-1):
+				word = words[i]
+				if not word in temp:
+					temp[word] = {}
+				temp = temp[word]
+			word = words[n-1]
+		else:
+			word = words[0]
+		temp[word] = phrase
+	fr.close()
+
+	# "__ __ __ __ __ support vector machine __ __ __ __ __"
+	n_context = 5
+	phrase2classifiers = {}
+	for [paperid,path] in paperid_path:
+		fr = open(path,'rb')
+		for line in fr:
+			words = line.strip('\r\n').split(' ')
+			wordslower = line.strip('\r\n').lower().split(' ')
+			l = len(words)
+			i = 0
+			while i < l:
+				isvalid = False
+				for j in range(min(nindex,l-i),0,-1):
+					temp = index[j-1]
+					k = 0
+					while k < j and i+k < l:
+						tempword = wordslower[i+k]
+						if not tempword in temp: break
+						temp = temp[tempword]
+						k += 1
+					if k == j:
+						phrase = temp
+						if not phrase in phrase2classifiers:
+							phrase2classifiers[phrase] = [0,[[0 for t in range(0,n_type)] for c in range(0,n_context)]]
+						phrase2classifiers[phrase][0] += 1
+						for c in range(0,n_context):
+							if i-1-c >= 0:
+								trigger = wordslower[i-1-c]
+								for t in range(0,n_type):
+									if trigger in wordsets[t]:
+										phrase2classifiers[phrase][1][c][t] += 1
+#										for _c in range(c,n_context):
+#											phrase2classifiers[phrase][1][_c][t] += 1
+							if i+k+c < l:
+								trigger = wordslower[i+k+c]
+								for t in range(0,n_type):
+									if trigger in wordsets[t]:
+										phrase2classifiers[phrase][1][c][t] += 1
+#										for _c in range(c,n_context):
+#											phrase2classifiers[phrase][1][_c][t] += 1
+						isvalid = True
+						break
+				if isvalid:
+					i += j
+					continue
+				i += 1
+		fr.close()
+	fw = open('entitytyping.txt','w')
+	s = 'ENTITY\tCOUNT'
+	for c in range(0,n_context): s += '\tWINDOWSIZE'+str(c+1)
+	fw.write(s+'\n')
+	for [phrase,[count,ctmatrix]] in sorted(phrase2classifiers.items(),key=lambda x:-x[1][0]):
+		s = phrase+'\t'+str(count)
+		for c in range(0,n_context):
+			maxv,maxt = -1,-1
+			for t in range(0,n_type):
+				v = ctmatrix[c][t]
+				if v > maxv:
+					maxv = v
+					maxt = t
+			if maxv == 0:
+				s += '\tN/A'
+			else:
+				s += '\t'+types[maxt]
+				for t in range(0,n_type):
+					v = ctmatrix[c][t]
+					if v == 0: continue
+					s += ' '+types[t][0]+types[t][-1]+':'+str(v)
+		fw.write(s+'\n')
+	fw.close()
 	
 class Paper(object):
 	def __init__(self, pid, title, year, conf):
@@ -693,12 +805,15 @@ if __name__ == '__main__':
 		# print ruleleft,'-->',ruleright,support,confidence
 	# print 'Number of rules:',len(rules)
 	
-	# Task 2: Entity Name Detection (paperclassification.py functions)
+	# Task 2: Entity Mining - Name Detection (paperclassification.py functions)
 	SimpleEntityExtraction()
 	SimpleAttributeExtraction()
 	SimpleLabelExtraction()
 	DecisionTreeFirstFeature()
 	NaiveBayes()
+
+	# Task 3: Entity Typing (entitytyping.py functions)
+	SimpleEntityTyping()
 
 	'''
 	# Task 4: Finding two-four authors that often collaborate
