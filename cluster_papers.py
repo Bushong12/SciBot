9a98504f6bd97c4520be0e4ffb09e32f43f7f56a
Catherine Badart <cbadart@DESKTOP-0V8P6TL.localdomain>
2017-12-04 20:03:09 -0500

    cleaned up integrate.py a little and got started on paper clustering

---
+++ cluster_papers.py
@@ -0,0 +1,9 @@
+from integrate import Paper
+import os
+import sys
+from sklearn.cluster import KMeans
+import numpy as np
+
+#################### TASK 7: PAPER CLUSTERING ####################
+if __name__ == '__main__':
+	print("Working")
--- integrate.py
+++ integrate.py
@@ -585,6 +585,7 @@ if __name__ == '__main__':
 	
 	# initialize the dictionaries 
 	author_names = init_author_dict()
+	print('Number of authors: {}'.format(str(len(author_names))))
 	paper_author = init_paper_author_dict()
 	paper_keywords = init_paper_keywords_dict()
 	index_dict = init_index_dict()
@@ -604,7 +605,7 @@ if __name__ == '__main__':
 	
 	# CSV header
 	integrated_data.write("PID,PDFID,title,conf,folder,year,affil,authors,author_ids,keywords\n")
-	
+	num_papers = 0
 	# process Papers
 	for line in open('microsoft/Papers.txt'):
 		data = line.rstrip().split('\t')
@@ -643,185 +644,19 @@ if __name__ == '__main__':
 													  concat_list(paper.keywords)
 													)
 		integrated_data.write(paper_data)
-
+		num_papers += 1
 		
-	################################## MOVE TO NEW FILE ######################################
-	# file for storing all the keywords that we generate
-	new_keywords = open('data/our_keywords.txt', 'w+')
-	# file for keyword bigrams
-	bigram_file = open('data/keyword_bigrams.txt', 'w+')
-	# file for storing bigram transactions
-	transaction_file = open('data/keyword_transactions.txt', 'w+')
-	# file for result of Apriori
-	apriori_file = open('data/keyword_apriori.txt', 'w+')
-	# file for result of non-singe itemsets
-	nonsingle_file = open('data/keyword_counts.txt', 'w+')
-	
-	all_files = list()
-	# walk through all directories
-	print("Collecting words from documents...")
-	file_list = list()
-	test_files = list()
-	num_test = 0
-	for root, dirs, files in os.walk("text"):
-		for subdir in dirs:
-			dir_path = os.path.join(root, subdir)
-			#print(file_path)
-			for sub_root, sub_dirs, sub_files in os.walk(dir_path):
-				for file_name in sub_files:
-					file_path = os.path.join(dir_path, file_name)
-					#print(file_path)
-					file_list.append(file_path)
-					if num_test < 10:
-						test_files.append(file_path)
-						num_test += 1
-
-	# create list of lists of all important words in the documents (ResponseBot 1)
-	for doc in file_list:
-		for line in open(doc):
-			words = tokenizer(line.rstrip())
-			allwords.append(words)
-	#for words in allwords: print words
-
-	# find the support for each entity candidate (ResponseBot 2)
-	word2support = {}
-	print("Calculating support for each word...")
-	#sys.stdout.write("Calculating support for each word.")
-	for words in allwords:
-		for word in set(words):
-			if word in stopwords or len(word) == 1 or word.isdigit(): continue
-			if not word.isalnum(): continue
-			if not word in word2support:
-				word2support[word] = 0
-			word2support[word] += 1
-	# output
-	for [word,support] in sorted(word2support.items(),key=lambda x:-x[1]):
-		if support == 1: break
-		new_keywords.write('{} {}\n'.format(word, support))
-		#sys.stdout.write('.')
-	#print('\n')
-	print("Found {} entity candidates".format(str(len(word2support))))
-	# Find bigram for each entity candidate (ResponseBot 3)
-	word2count = {}
-	print("Finding Bigrams...")
-	for words in allwords:
-		for word in words:
-			if word in stopwords or len(word) == 1 or word.isdigit(): continue
-			if not word.isalnum(): continue
-			if not word in word2count:
-				word2count[word] = 0
-			word2count[word] += 1
-	bigram2score = {} # bigram's count, first word's count, second word's count, significance score
-	L = 0
-	print("Counting Bigrams...")
-	#sys.stdout.write("Counting Bigrams.")
-	for words in allwords:
-		n = len(words)
-		L += n
-		for i in range(0, n-1):
-			if words[i] in word2count and words[i+1] in word2count:
-				bigram = words[i] + '_' + words[i+1]
-				if not bigram in bigram2score:
-					bigram2score[bigram] = [0,word2count[words[i]],word2count[words[i+1]],0.0]
-				bigram2score[bigram][0] += 1
-	for bigram in bigram2score:
-		bigram2score[bigram][3] = (1.0*bigram2score[bigram][0]-1.0*bigram2score[bigram][1]*bigram2score[bigram][2]/L)/((1.0*bigram2score[bigram][0])**0.5)
-	# output
-	bigram_file.write('bigram,count-bigram,count-first-word,count-second-word,significance-score\n')
-	for [bigram,score] in sorted(bigram2score.items(),key=lambda x:-x[1][3]):
-		bigram_file.write('{} {} {} {} {}\n'.format(bigram, score[0], score[1], score[2], score[3]))
-		#sys.stdout.write('.')
-	print("Found {} bigrams".format(str(len(bigram2score))))
-
-	# find transactions for bigrams (ResponseBot 4)
-	print("Finding Transactions...")
-	#sys.stdout.write("Finding Transactions")
-	bigramdict = {}
-	for bigram in bigram2score:
-		if bigram2score[bigram][0] > 1:
-			#[firstword,secondword] = bigram.split('_')
-			firstword = bigram.split('_')[0]
-			secondword = bigram.split('_')[0]
-			#print bigram.split('_')
-			if not firstword in bigramdict:
-				bigramdict[firstword] = set()
-			bigramdict[firstword].add(secondword)
-	transactions = [] # response
-	for words in allwords:
-		transaction = set() # set of words/bigrams
-		n = len(words)
-		i = 0
-		while i < n:
-			if words[i] in bigramdict and i+1 < n and words[i+1] in bigramdict[words[i]]:
-				transaction.add(words[i]+'_'+words[i+1])
-				i += 2
-				continue
-			if words[i] in stopwords or len(words[i]) == 1 or words[i].isdigit():
-				i += 1
-				continue
-			transaction.add(words[i])
-			i += 1
-		transactions.append(list(transaction))
-	print("Found {} transactions".format(str(len(transactions))))
-	# output
-	for transaction in transactions:
-		if not transaction: continue
-		t = ','.join(transaction)
-		#if check_string_guality(t) is False: continue
-		#t = t.replace('#', '')
-		#t = t.replace('*', '')
-		transaction_file.write('{}\n'.format(t))
-		#sys.stdout.write('.')
-
-	print("Preforming Apriori...")
-	# Apriori (ResponseBot 5)
-	# http://www.borgelt.net/pyfim.html	
-	patterns = apriori(transactions,supp=-1000) # +: percentage -: absolute number
-	#print(type(patterns)) -> patterns is a list
-	# output
-	for (pattern,support) in sorted(patterns,key=lambda x:-x[1]):
-		#print(type(pattern)) -> pattern is a tuple
-		if len(set(pattern)) <= 1: continue
-		p = ','.join(pattern)
-		apriori_file.write('{} {} \n'.format(p, str(support)))
-	apriori_file.write('Number of patterns: {}\n'.format(len(patterns)))
-	print('Number of patterns: {}\n'.format(len(patterns)))
-
-	#FP-Growth (ResponseBot 6)
-	# patterns = fpgrowth(transactions,supp=-3)
-	#output
-	# fp_file.write('-------- FP-Growth --------\n')
-	# for (pattern,support) in sorted(patterns,key=lambda x:-x[1]):
-		# fp_file.write('{} {}\n'.format(pattern, support)
-	# fp_file.write('Number of patterns: {}'.format(len(patterns)))
-
-	#Closed Non-Single Itemsets (ResponseBot 7)
-	print("Building non-single itemsets with FP-Growth...")
-	patterns = fpgrowth(transactions,target='c',supp=-1000,zmin=2)
-	#output
-	for (pattern,support) in sorted(patterns,key=lambda x:-x[1]):
-		p = ','.join(pattern)
-		nonsingle_file.write('{} {} \n'.format(p, str(support)))
-	print 'Number of patterns:',len(patterns)
-
-
-	#One-to-Many Association Rules (ResponseBot 8)
-	# rules = apriori(transactions,target='r',supp=-2,conf=70,report='sc')
-	#output
-	# print '-------- One-to-Many Association Rules --------'
-	# for (ruleleft,ruleright,support,confidence) in sorted(rules,key=lambda x:x[0]):
-		# print ruleleft,'-->',ruleright,support,confidence
-	# print 'Number of rules:',len(rules)
+	print('Number of papers: {}'.format(str(num_papers)))
 	
 	# Task 2: Entity Mining - Name Detection (paperclassification.py functions)
 	SimpleEntityExtraction()
-	#SimpleAttributeExtraction()
-	#SimpleLabelExtraction()
-	#DecisionTreeFirstFeature()
-	#NaiveBayes()
+	SimpleAttributeExtraction()
+	SimpleLabelExtraction()
+	DecisionTreeFirstFeature()
+	NaiveBayes()
 
 	# Task 3: Entity Typing (entitytyping.py functions)
-	SimpleEntityTyping() # must run SimpleEntityExtraction before this code
+	#SimpleEntityTyping() # must run SimpleEntityExtraction before this code
 
 	'''
 	# Task 4: Finding two-four authors that often collaborate
@@ -926,6 +761,4 @@ if __name__ == '__main__':
 		print pattern,support
 	print 'Number of patterns:',len(patterns)
 	'''
-
-	
 	print("Done.")
