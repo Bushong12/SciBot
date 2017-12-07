ccfbd670d6fea98aeae85d0d4aac7113ba8dcc0b
gbushong <grace@phoenix.com>
2017-12-06 20:00:47 -0500

    updated files for each task

---
+++ task1.py
@@ -0,0 +1,159 @@
+import sys
+import os
+
+def init_author_dict():
+    # maps AID to author name
+    names = dict()
+    for line in open('microsoft/Authors.txt'):
+        data = line.rstrip().split('\t')
+        AID = data[0]
+        name = data[1]
+        names[AID] = name
+    return names
+        
+def init_paper_author_dict():
+    # maps PID to list of authors that are a pair of (author_id, affiliation)
+    d = dict()
+    for line in open('microsoft/PaperAuthorAffiliations.txt'):
+        data = line.rstrip().split('\t')
+        PID = data[0]
+        AID = data[1]
+        aff = data[4]
+        seq_num = data[5]
+        author_obj = [AID, aff]
+        if PID not in d:
+            d[PID] = list()
+        d[PID].append(author_obj)
+    return d        
+
+def init_paper_keywords_dict():
+    # maps PID to set of keywords found in PaperKeywords.txt
+    keywords = dict()
+    for line in open('microsoft/PaperKeywords.txt'):
+        data = line.rstrip().split('\t')
+        PID = data[0]
+        word = data[1]
+        if PID not in keywords:
+            keywords[PID] = set()
+        keywords[PID].add(word)
+    return keywords
+
+def init_index_dict():
+    # maps PID to (PDFID, folder)
+    index = dict()
+    for line in open('microsoft/index.txt'):
+        data = line.rstrip().split('\t')
+        folder = data[0]
+        PDFID = data[1]
+        PID = data[2]
+        index[PID] = [PDFID, folder]
+    return index
+
+class Paper(object):
+    def __init__(self, pid, title, year, conf):
+        self.PID = pid
+        self.authors = set()
+        self.author_ids = set()
+        self.keywords = set()
+        self.conf = conf
+        self.year = year
+        self.title = title
+        try:
+            self.PDFID = index_dict[self.PID][0]
+            self.folder = index_dict[self.PID][1]
+        except KeyError:
+            self.PDFID = 'na'
+            self.folder = 'na'
+        self.affil = ''
+        
+    def make_author_sets(self):
+        # paper_author maps PID to (author_id, affiliation)
+        author_pairs = paper_author[self.PID]
+        for pair in author_pairs:
+            id = pair[0]
+            self.author_ids.add(id)
+        # author_names maps AID to author name
+        for id in self.author_ids:
+            name = author_names[id]
+            self.authors.add(name)
+    
+    def set_affil(self, affil_dict):
+        # uses paper_author dict PID:(AID, affil)
+        author_affils = affil_dict[self.PID]
+        self.affil = author_affils[0][1]
+    
+    def make_keyword_set(self):
+        try:
+            self.keywords = paper_keywords[self.PID]
+        except KeyError:
+            self.keywords.add('na')
+
+
+if __name__ == '__main__':
+    # csv for PID,PDFID,title,conf,folder,year,affil,authors,author_ids,keywords
+    integrated_data = open('data/integrated.csv', 'w+')
+    
+    # initialize the dictionaries 
+    author_names = init_author_dict()
+    print('Number of authors: {}'.format(str(len(author_names))))
+    paper_author = init_paper_author_dict()
+    paper_keywords = init_paper_keywords_dict()
+    index_dict = init_index_dict()
+    entity_candidates = dict()
+    allwords = [] # list containing the important words from each document that we'd want to scan through
+    stopwords = set()
+
+    # create set of entity candidates
+    for key in paper_keywords:
+        for word in paper_keywords[key]:
+            entity_candidates[word] = 0
+
+    # create set of stopwords - ResponseBot #2
+    for line in open('microsoft/stopwords.txt'):
+        word = line.strip('\r\n').lower()
+        stopwords.add(word)
+
+    # CSV header
+    integrated_data.write("PID,PDFID,title,conf,folder,year,affil,authors,author_ids,keywords\n")
+    num_papers = 0
+    # process Papers
+    for line in open('microsoft/Papers.txt'):
+        data = line.rstrip().split('\t')
+        # parse data
+        PID = data[0]       
+        title = data[2]
+        year = int(data[3])
+        conf = data[7]
+        
+        # error checking 
+        confs = {'icdm', 'kdd', 'wsdm', 'www'}
+        if conf not in confs:
+            continue
+        if year > 2016 or year < 1994:
+            continue
+            
+        # make new Paper object
+        paper = Paper(PID, title, year, conf)
+        paper.make_author_sets()
+        paper.set_affil(paper_author)
+        temp_key = paper.make_keyword_set()
+        
+        if paper.PDFID == 'na':
+            continue
+        
+        # output all the data 
+        paper_data = "{},{},{},{},{},{},{},{},{},{}\n".format(paper.PID, 
+                                                      paper.PDFID, 
+                                                      paper.title, 
+                                                      paper.conf,
+                                                      paper.folder,
+                                                      paper.year,
+                                                      paper.affil,
+                                                      concat_list(paper.authors),
+                                                      concat_list(paper.author_ids),
+                                                      concat_list(paper.keywords)
+                                                    )
+        integrated_data.write(paper_data)
+        num_papers += 1
+        
+    print('Number of papers: {}'.format(str(num_papers)))
---
+++ task4.py
@@ -0,0 +1,136 @@
+import sys
+import os
+from fim import apriori
+
+def init_author_dict():
+    # maps AID to author name
+    names = dict()
+    for line in open('microsoft/Authors.txt'):
+        data = line.rstrip().split('\t')
+        AID = data[0]
+        name = data[1]
+        names[AID] = name
+    return names
+    
+def init_paper_author_dict():
+    # maps PID to list of authors that are a pair of (author_id, affiliation)
+    d = dict()
+    for line in open('microsoft/PaperAuthorAffiliations.txt'):
+        data = line.rstrip().split('\t')
+        PID = data[0]
+        AID = data[1]
+        aff = data[4]
+        seq_num = data[5]
+        author_obj = [AID, aff]
+        if PID not in d:
+            d[PID] = list()
+        d[PID].append(author_obj)
+    return d        
+
+if __name__ == '__main__':
+    author_names = init_author_dict()
+    paper_author = init_paper_author_dict()
+
+    # Task 4: Finding two-four authors that often collaborate
+    # basically we'll do a similar thing we already did with the keywords with the author data
+    author_lists = [] # list of lists of authors on each paper
+    aid_lists = [] # list of lists of author id's on each paper
+    for key in paper_author:
+        author_list = []
+        aid_list = []
+        for each in paper_author[key]:
+            aid_list.append(each[0])
+            name = author_names[each[0]]
+            author_list.append(name)
+        #print author_list
+        #print aid_list
+        author_lists.append(author_list)
+        aid_lists.append(aid_list)
+
+    # find the support for each entity candidate (ResponseBot 2)
+    author2support = {}
+    for words in author_lists:
+        for word in words:
+            #if word in stopwords or len(word) == 1 or word.isdigit() or len(word) > 25: continue
+            #if not word.isalnum(): continue
+            if not word in author2support:
+                author2support[word] = 0
+            author2support[word] += 1
+    # output
+    #for [word,support] in sorted(author2support.items(),key=lambda x:-x[1]):
+    #   if support == 1: break
+    #   print word,support
+
+    # Find bigram for each entity candidate (ResponseBot 3)
+    author2count = {}
+    for words in author_lists:
+        for word in words:
+            #if word in stopwords or len(word) == 1 or word.isdigit() or len(word) > 25: continue
+            #if not word.isalnum(): continue
+            if not word in author2count:
+                author2count[word] = 0
+            author2count[word] += 1
+
+    abigram2score = {} # bigram's count, first word's count, second word's count, significance score
+    L = 0
+    for words in author_lists:
+        n = len(words)
+        L += n
+        for i in range(0, n-1):
+            if words[i] in author2count and words[i+1] in author2count:
+                bigram = words[i] + '_' + words[i+1]
+                if not bigram in abigram2score:
+                    abigram2score[bigram] = [0,author2count[words[i]],author2count[words[i+1]],0.0]
+                abigram2score[bigram][0] += 1
+    for bigram in abigram2score:
+        abigram2score[bigram][3] = (1.0*abigram2score[bigram][0]-1.0*abigram2score[bigram][1]*abigram2score[bigram][2]/L)/((1.0*abigram2score[bigram][0])**0.5)
+    # output
+    #print 'bigram', 'count-bigram', 'count-first-word','count-second-word','significance-score'
+    #for [bigram,score] in sorted(abigram2score.items(),key=lambda x:-x[1][3]):
+    #   print bigram,score[0],score[1],score[2],score[3]
+
+    # find transactions for bigrams (ResponseBot 4)
+    abigramdict = {}
+    for bigram in abigram2score:
+        if abigram2score[bigram][0] > 1:
+            #[firstword,secondword] = bigram.split('_')
+            firstword = bigram.split('_')[0]
+            secondword = bigram.split('_')[0]
+            #print bigram.split('_')
+            if not firstword in abigramdict:
+                abigramdict[firstword] = set()
+            abigramdict[firstword].add(secondword)
+
+    author_transactions = [] # response
+    for words in author_lists:
+        transaction = set() # set of words/bigrams
+        n = len(words)
+        i = 0
+        while i < n:
+            if words[i] in abigramdict and i+1 < n and words[i+1] in abigramdict[words[i]]:
+                transaction.add(words[i]+'_'+words[i+1])
+                i += 2
+                continue
+            #if words[i] in stopwords or len(words[i]) == 1 or words[i].isdigit() or not words[i].isalnum() or len(words[i]) > 25:
+            #   i += 1
+            #   continue
+            transaction.add(words[i])
+            i += 1
+        if not transaction: continue
+        author_transactions.append(list(transaction))
+    # output
+    #for transaction in author_transactions:
+    #   print transaction
+
+    print "Done with author transactions"
+
+
+    #patterns = apriori(author_lists, supp=-12)
+    patterns = apriori(author_transactions, supp=-3)
+    print '-------- Author Affiliation Apriori --------'
+    for (pattern,support) in sorted(patterns,key=lambda x:-x[1]):
+        if len(pattern) <= 1: continue
+        print pattern,support
+    print 'Number of patterns:',len(patterns)
+
+
